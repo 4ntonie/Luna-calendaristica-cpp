1ca89c11387116aeac48f02c172ff5f198ecbf02
Antonie Soptica <95506120+4ntonie@users.noreply.github.com>
2021-12-11 18:33:37 +0200

    #2606 luniramase
    Cerința
    Se citește de la tastatură un număr natural ce reprezintă o lună a anului (dacă citim 3 ne referim la a treia lună, deci Martie). Programul va afișa (câte unul pe rând) numele lunilor rămase în acel an după cea citită. Se garantează că se introduce de la intrare o valoare cuprinsă între 1 și 11. Se recomandă utilizarea instrucțiunii switch.
    
    Date de intrare
    Programul citește de la tastatură un număr l.
    
    Date de ieșire
    Programul va afișa, câte una pe rând, cu inițială mare, lunile rămase după cea cu numărul citit.
    
    Restricții și precizări
    1 ≤ l ≤ 11
    
    Exemplu
    Intrare
    
    9
    Ieșire
    
    Octombrie
    Noiembrie
    Decembrie

---
+++ luniramase.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    switch(n)
+    {
+        case 1:
+            cout<<"Februarie"<<"\n";
+            cout<<"Martie"<<"\n";
+            cout<<"Aprilie"<<"\n";
+            cout<<"Mai"<<"\n";
+            cout<<"Iunie"<<"\n";
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n";
+            break;
+        case 2:
+            cout<<"Martie"<<"\n";
+            cout<<"Aprilie"<<"\n";
+            cout<<"Mai"<<"\n";
+            cout<<"Iunie"<<"\n";
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 3:
+            cout<<"Aprilie"<<"\n";
+            cout<<"Mai"<<"\n";
+            cout<<"Iunie"<<"\n";
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 4:
+            cout<<"Mai"<<"\n";
+            cout<<"Iunie"<<"\n";
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 5:
+            cout<<"Iunie"<<"\n";
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 6:
+            cout<<"Iulie"<<"\n";
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 7:
+            cout<<"August"<<"\n";
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 8:
+            cout<<"Septembrie"<<"\n";
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 9:
+            cout<<"Octombrie"<<"\n";
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 10:
+            cout<<"Noiembrie"<<"\n";
+            cout<<"Decembrie"<<"\n"; break;
+        case 11:
+            cout<<"Decembrie"<<"\n";
+        
+    }
+    return 0;
+}
